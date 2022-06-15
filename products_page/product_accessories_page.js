dafa4b8cf7f6e62e1d4832c6b0d83f0dc3db10df
sukanta255 <sukantap872@gmail.com>
2022-06-15 23:47:02 +0530

    products page added

---
+++ products_page/product_accessories_page.js
@@ -0,0 +1,326 @@
+var fashionData = [
+    {
+      avatar:
+        "https://images.bewakoof.com/t320/men-s-white-lace-ups-sneakers-499234-1652707677-1.jpg",
+        brand : "Bewakoof",
+      name: "Men's White Lace Up Sneakers",
+      rupees:"₹",
+      price: "299",
+      rupees2:"₹",
+      price2: "999",
+      offer : "₹279 For TriBe Members"
+     
+       },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-white-and-black-color-block-sneakers-479855-1646294405-1.jpg",
+       
+        brand : "Bewakoof",
+      name: "Men's Black Lace Up Sneakers",
+      rupees:"₹",
+      price: "499",
+      rupees2:"₹",
+      price2: "1299",
+      offer : "₹469 For TriBe Members",
+      
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-orange-and-black-color-block-casual-shoes-479836-1646293843-1.jpg",
+        brand : "Bewakoof",
+      name: "Men's White Lace Up Sneakers",
+      rupees:"₹",
+      price: "299",
+      rupees2:"₹",
+      price2: "999",
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-green-and-white-color-block-sneakers-479844-1646294494-1.jpg",
+        brand : "Bewakoof",
+      name: "Men's White Lace Up Sneakers",
+      rupees:"₹",
+      price: "499",
+      rupees2:"₹",
+      price2: "1299",
+      offer : "₹469 For TriBe Members"
+    
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-blue-color-block-lace-ups-casual-shoes-489318-1649678372-1.jpg",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "399",
+      rupees2:"₹",
+      price2: "999",
+      offer : "₹379 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-blue-and-white-color-block-sneakers-479862-1646294556-1.jpg",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "499",
+      rupees2:"₹",
+      price2: "830",
+      offer : "₹459 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-green-and-black-color-block-casual-shoes-479834-1646294134-1.jpg",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "499",
+      rupees2:"₹",
+      price2: "999",
+      offer : "₹449 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-black-lace-ups-casual-shoes-499241-1652707726-1.jpg",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "449",
+      rupees2:"₹",
+      price2: "799",
+      offer : "₹419 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-white-mesh-lace-ups-sneakers-485541-1649140736-1.JPG",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "499",
+      rupees2:"₹",
+      price2: "1299",
+      offer : "₹459 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-white-color-block-casual-shoes-479839-1646294018-1.jpg",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "399",
+      rupees2:"₹",
+      price2: "1099",
+      offer : "₹379 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/woakers-men-s-multicolor-casual-shoes-453392-1641915679-1.jpg",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "349",
+      rupees2:"₹",
+      price2: "999",
+      offer : "₹329 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/afrojack-men-s-white-blue-color-block-sneakers-505771-1654185378-1.JPG",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "349",
+      rupees2:"₹",
+      price2: "999",
+      offer : "₹329 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-white-grey-color-block-lightweight-casual-shoes-496988-1651826939-1.jpg",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "299",
+      rupees2:"₹",
+      price2: "1099",
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/afrojack-men-s-black-self-design-sneakers-505757-1654184979-1.JPG",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "299",
+      rupees2:"₹",
+      price2: "1299",
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-olive-lace-ups-sneakers-499226-1652707453-1.jpg",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "299",
+      rupees2:"₹",
+      price2: "999",
+      offer : "₹279 For TriBe Members"
+    },
+    
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-white-red-color-block-lace-ups-sneakers-485542-1649140858-1.jpg",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "299",
+      rupees2:"₹",
+      price2: "1249",
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-white-lightweight-casual-shoes-496991-1651826912-1.jpg",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "299",
+      rupees2:"₹",
+      price2: "1099",
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-red-blue-color-block-sports-shoes-499231-1652707954-1.jpg",
+         brand : "Bewakoof",
+         name: "Men's White Lace Up Sneakers",
+         rupees:"₹",
+      price: "299",
+      rupees2:"₹",
+      price2: "830",
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-black-high-top-slip-on-sneakers-485526-1649140563-1.jpg",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "299",
+      rupees2:"₹",
+      price2: "1299",
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/afrojack-men-s-black-high-top-sneakers-505818-1654185177-1.JPG",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "299",
+      rupees2:"₹",
+      price2: "1299",
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/men-s-brown-premium-casual-shoes-509657-1655123203-1.JPG",
+        brand : "Bewakoof",
+        name: "Men's White Lace Up Sneakers",
+        rupees:"₹",
+      price: "299",
+      rupees2:"₹",
+      price2: "1099",
+      offer : "₹279 For TriBe Members"
+    },
+    
+    
+  ];
+
+  //fashionArr=JSON.parse(localStorage.getItem("dataone"))||[];
+  
+displayData(fashionData)
+//console.log (fashionArr)
+
+
+
+function displayData(data){
+  fashionData.forEach(function (elem) {
+    //console.log (elem.name)
+    var box = document.createElement("div");
+    var fashion_avatar = document.createElement("img");
+    fashion_avatar.setAttribute("src", elem.avatar);
+    fashion_avatar.style.cursor="pointer";
+     var  fashion_brand = document.createElement("h3");
+    fashion_brand.innerText = elem.brand;
+    var fashion_name = document.createElement("p");
+    fashion_name.innerText = elem.name;
+
+    var div5 = document.createElement("div");
+    var fashion_rupees = document.createElement("h3");
+    fashion_rupees.innerText = elem.rupees;
+    fashion_rupees.style.color = "black";
+    div5.append(fashion_rupees)
+
+    var div1 = document.createElement("div");
+    var fashion_price = document.createElement("h3");
+    fashion_price.innerText = elem.price;
+    fashion_price.style.color = "black";
+    div1.append(fashion_price)
+
+    var div6 = document.createElement("div");
+    var fashion_rupees2 = document.createElement("h4");
+    fashion_rupees2.innerText = elem.rupees2;
+    div6.append(fashion_rupees2)
+
+    var fashion_price2 = document.createElement("h4");
+    fashion_price2.innerText = elem.price2;
+    var div2 = document.createElement("div");
+    div2.append(fashion_price2)
+
+    var div3 = document.createElement("div");
+    div3.append(div5,div1,div6,div2);
+
+    
+    // fashion_price2.style.color = "red"
+    var fashion_offer = document.createElement("h4");
+    fashion_offer.innerText = elem.offer;
+    
+    // var six=document.createElement("button");
+    // six.innerText="Click Here For Details";
+    // six.style.cursor="pointer";
+
+    // six.addEventListener("click",function(){
+    //     press(elem);
+    // })
+
+    fashion_avatar.addEventListener("click",function(){
+            press(elem);
+        })
+    // box.append(fashion_avatar,  fashion_brand, fashion_name , fashion_price, fashion_offer,six);
+    box.append(fashion_avatar, fashion_brand, fashion_name, div3, fashion_offer);
+    document.querySelector("#container").append(box);
+    //console.log (elem)
+
+
+  })
+}
+function press(elem){
+  document.querySelector("#container").innerHTML=""
+ // fashionArr.push(elem);
+  localStorage.setItem("dataone",JSON.stringify(elem));
+  window.location.href="../product2/addtocart.html"
+}
+
+
+
+  
+
+   
+
+
+
+ 
+  
